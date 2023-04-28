59bf001efa086f539edab3d7864e151cf50f2528
commandstring <bmxmikey2@gmail.com>
2023-04-28 11:08:22 -0400

    Added repositories page

--- App/Routes.php
+++ App/Routes.php
@@ -8,6 +8,10 @@ Router::get("/", Render::class)
     ->addStaticArgument("path", "home")
 ;
 
+Router::get("/repositories", Render::class)
+    ->addStaticArgument("path", "repos")
+;
+
 Router::catch(HttpNotFound::class, Render::class)
     ->addStaticArgument("path", "404")
     ->addStaticArgument("context", ["uri" => $_SERVER['REQUEST_URI']])
